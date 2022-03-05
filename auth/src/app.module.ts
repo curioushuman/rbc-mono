5f6e7995e82fc63f3ecc88dca55d4d6c38da9993
Michael Kelly <mike@curioushuman.com.au>
2022-03-05 17:30:33 +1100

    Adding in the configuration service

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AutomapperModule } from '@automapper/nestjs';
 import { classes as AutomapperClasses } from '@automapper/classes';
 
+import databaseConfig from './config/database.config';
 import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://auth-mongo-srv/auth'),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      load: [databaseConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
     AutomapperModule.forRoot({
       options: [{ name: 'Mapper', pluginInitializer: AutomapperClasses }],
       singular: true,
