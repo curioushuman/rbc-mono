81e4c9f60379aab2bb1a13dc9af962ba6f9ef2d1
Michael Kelly <mike@curioushuman.com.au>
2022-03-08 11:33:24 +1100

    Improved config handling for kafka microservices

--- subscriptions/src/bootstrap/bootstrap.ts
+++ subscriptions/src/bootstrap/bootstrap.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import type { KafkaConfig } from '@curioushuman/rbc-common';
+
+import { KafkaConsumerConfig } from '../microservices/types/kafka.consumer.config';
 
 export class App {
   public static async start(module: any) {
@@ -9,18 +12,16 @@ export class App {
   }
 
   public static async setup(app: INestApplication) {
+    // grab config
+    const configService = app.get(ConfigService);
+
+    // grab subscriptions microservice config
+    const subscriptionsConfig = configService.get<KafkaConfig>(
+      'microservices.services.subscriptions',
+    );
+
     // add microservices
-    app.connectMicroservice({
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: ['kafka-srv:9092'],
-        },
-        consumer: {
-          groupId: 'subscriptions-consumer',
-        },
-      },
-    });
+    app.connectMicroservice(new KafkaConsumerConfig(subscriptionsConfig).get());
 
     // start the service
     await app.startAllMicroservices();
