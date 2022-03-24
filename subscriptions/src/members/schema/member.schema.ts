e05a9eed623bbfcc6e8201de485b745f8bc9d255
Michael Kelly <mike@curioushuman.com.au>
2022-03-24 14:01:18 +1100

    rbc-subscriptions: adding testing for members

--- subscriptions/src/members/schema/member.schema.ts
+++ subscriptions/src/members/schema/member.schema.ts
@@ -1,15 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types, Model } from 'mongoose';
-import { Expose, Transform } from 'class-transformer';
 
 @Schema()
 export class Member {
-  @Expose()
   @Prop({ type: Types.ObjectId, required: true, unique: true })
   id!: string;
 
-  @Transform(({ obj }) => 'overidden@transform.dto')
-  @Expose()
   @Prop({ required: true })
   email!: string;
 }
